eb1e2011ecefdf3f337a03f2cc10094881165b80
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2023-03-31 10:53:53 +0700

    int array

--- Quicksort_162.cpp
+++ Quicksort_162.cpp
@@ -1,2 +1,8 @@
 #include <iostream>
 using namespace std;
+
+//array of integres to hold values
+int arr[20];
+int cmp_count = 0; // number of comparasion
+int moc_count = 0; // number of movement
+int n;
