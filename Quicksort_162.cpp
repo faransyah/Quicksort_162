4c5eabd0e57d1a1445e5448176080bdc201cbe7b
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2023-03-31 10:56:18 +0700

    void input

--- Quicksort_162.cpp
+++ Quicksort_162.cpp
@@ -6,3 +6,17 @@ int arr[20];
 int cmp_count = 0; // number of comparasion
 int moc_count = 0; // number of movement
 int n;
+void input() {
+	while (true)
+	{
+		cout << "masukan panjang element array :";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\n Maksimum panjang array adalah 20" << endl;
+
+	}
+
+}
